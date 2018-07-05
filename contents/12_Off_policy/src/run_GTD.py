a6a44b647d72f8110b8664fd9c20c79188f914b0
hzm2016 <2539722953@qq.com>
2018-07-05 09:32:15 -0600

    update mainfile

--- contents/12_Off_policy/src/run_GTD.py
+++ contents/12_Off_policy/src/run_GTD.py
@@ -12,12 +12,16 @@ from algorithms import GTD
 def main(args):
     global siginfo_message
 
+    all_state = np.array([5, 15])
+    all_frequency = np.array([1000, 2500, 5000])
+    all_decay = np.array([0.99, 0.97, 0.95])
     all_agent = np.array([0, 1, 2, 3])
     all_rmse = np.ones((len(all_agent), args['num_runs'], args['num_seeds'], args['num_steps'])) * np.inf
     num_add = args['num_steps']/args['num_change'] - 1
     for option in range(len(all_agent)):
         for run in range(args['num_runs']):
             for seed in range(args['num_seeds']):
+
                 """build domain"""
                 domain = Ringworld(
                     args['num_states'],
@@ -33,20 +37,38 @@ def main(args):
                 learner = GTD(last_x)
                 for step in range(args['num_steps']):
 
+                    # # compute the frequency for every 1000 steps
+                    # if step % args['num_frequency'] == 0:
+                    #     fre_behavior = np.ones((args['num_states'], args['num_actions']))
+                    #
+                    # # compute the probability
+                    # fre_behavior[last_s, action] += 1
+
                     # compute the frequency for every 1000 steps
+
                     if step % args['num_frequency'] == 0:
-                        fre_behavior = np.ones((args['num_states'], args['num_actions']))
+                        fre_behavior = np.zeros((args['num_states'], args['num_actions']))
+
+                    # compute the probability
+                    fre_behavior[s, action] += 1
 
                     # update the probability
+                    # for i in range(args['num_actions']):
+                    #     if i == action:
+                    #         fre_count[last_s, i] += 1
+                    #     else:
+                    #         fre_count[last_s, i] *= args['decay']
+
                     for i in range(args['num_actions']):
                         if i == action:
-                            fre_count[last_s, i] += 1
+                            fre_count[s, i] += 1
                         else:
-                            fre_count[last_s, i] *= args['decay']
+                            fre_count[s, i] *= args['decay']
 
                     # update behavior policy
-                    if step > 0 & step % args['num_change'] == 0:
+                    if step > 0 and step % args['num_change'] == 0:
                         left_probability += (args['left_probability_end'] - args['left_probability'])/num_add
+                        print('left_probability', left_probability)
 
                     # set message for siginfo
                     siginfo_message = '[{0:3.2f}%] SEED: {1} of {2}, EPISODE: {3} of {4}, STEP: {5}'.format(
@@ -55,24 +77,28 @@ def main(args):
 
                     # compute in the same way
                     if option == 0:
+                        # print('unknown')
                         if action == domain.LEFT:
                             rho = 0.05 / args['left_probability']
                         else:
                             rho = 0.95 / (1 - args['left_probability'])
                     # compute with the changed probability
                     elif option == 1:
+                        # print('right rho')
                         if action == domain.LEFT:
                             rho = 0.05 / left_probability
                         else:
                             rho = 0.95 / (1 - left_probability)
                     # compute with the decay count
                     elif option == 2:
+                        # print('decay')
                         if action == domain.LEFT:
                             rho = 0.05 / (fre_count[s, action]/sum(fre_count[s, :]))
                         else:
                             rho = 0.95 / (fre_count[s, action]/sum(fre_count[s, :]))
                     # compute the count with a siding window
                     else:
+                        # print('count')
                         if action == domain.LEFT:
                             rho = 0.05 / (fre_behavior[s, action]/sum(fre_behavior[s, :]))
                             # print('0', fre_behavior[s, action]/sum(fre_behavior[s, :]))
@@ -187,7 +213,6 @@ def multiple_hyperparameters(args):
 
                 with open('{}/rmse_{}.npy'.format(args['directory'], args['test_name']), 'wb') as outfile:
                     np.save(outfile, all_rmse)
-
                 # with open('{}/lambda_{}_{}.npy'.format(args['directory'], args['test_name'], str(option)), 'wb') as outfile:
                 #     np.save(outfile, all_lambda)
 
@@ -198,19 +223,19 @@ def parse_args():
     parser.add_argument('--alpha', type=float, default=0.0005)
     parser.add_argument('--eta', type=float, default=0.05)
     parser.add_argument('--lambda', type=float, default=0.99)
-    parser.add_argument('--decay', type=float, default=0.90)
+    parser.add_argument('--decay', type=float, default=0.99)
     parser.add_argument('--ISW', type=int, default=0)
     parser.add_argument('--left_probability', type=float, dest='left_probability', default=0.05)
     parser.add_argument('--left_probability_end', type=float, dest='left_probability_end', default=0.95)
     parser.add_argument('--left_probability2', type=float, dest='left_probability2', default=0.75)
-    parser.add_argument('--num_seeds', type=int, dest='num_seeds', default=2)
-    parser.add_argument('--num_runs', type=int, dest='num_runs', default=100)
-    parser.add_argument('--num_states', type=int, dest='num_states', default=15)
+    parser.add_argument('--num_seeds', type=int, dest='num_seeds', default=100)
+    parser.add_argument('--num_runs', type=int, dest='num_runs', default=50)
+    parser.add_argument('--num_states', type=int, dest='num_states', default=5)
     parser.add_argument('--num_actions', type=int, dest='num_actions', default=2)
     parser.add_argument('--num_steps', type=int, dest='num_steps', default=50000)
     parser.add_argument('--num_frequency', type=int, dest='num_frequency', default=1000)
-    parser.add_argument('--num_change', type=int, dest='num_change', default=5000)  # every 5000 steps behavior policy add
-    parser.add_argument('--test_name', default='new_test')
+    parser.add_argument('--num_change', type=int, dest='num_change', default=1000)  # every 5000 steps behavior policy add
+    parser.add_argument('--test_name', default='four_agents_test')
     args = vars(parser.parse_args())
     if 'num_steps' not in args:
         args['num_steps'] = args['num_states'] * 100
@@ -237,6 +262,6 @@ if __name__ == '__main__':
     # rmse_filename = '{}/rmse_change_12.npy'.format(args['directory'])
     # lambda_filename = '{}/lambda_change_12.npy'.format(args['directory'])
     # if not (os.path.exists(rmse_filename) and (os.path.exists(lambda_filename))):
+    # multiple_hyperparameters(args)
 
     main(args)
-    # multiple_hyperparameters(args)
