8302c5912f793129689c3149ec0dd12f2983808e
hzm2016 <2539722953@qq.com>
2018-07-07 11:03:25 -0600

    update

--- contents/12_Off_policy/src/run_GTD.py
+++ contents/12_Off_policy/src/run_GTD.py
@@ -13,115 +13,122 @@ def main(args):
     global siginfo_message
 
     all_state = np.array([5, 15])
-    all_frequency = np.array([1000, 2500, 5000])
-    all_decay = np.array([0.99, 0.97, 0.95])
+    all_frequency = np.array([1000, 5000, 25000])
+    all_numchange = np.array([500, 1000, 5000, 10000])
+    all_decay = np.array([0.99, 0.80, 0.70])
     all_agent = np.array([0, 1, 2, 3])
-    all_rmse = np.ones((len(all_agent), args['num_runs'], args['num_seeds'], args['num_steps'])) * np.inf
-    num_add = args['num_steps']/args['num_change'] - 1
+    all_rmse = np.ones((len(all_agent), len(all_decay), len(all_numchange), args['num_runs'], args['num_seeds'], args['num_steps'])) * np.inf
     for option in range(len(all_agent)):
-        for run in range(args['num_runs']):
-            for seed in range(args['num_seeds']):
+        for decayInd, decay in enumerate(all_decay):
+            for changeInd, num_change in enumerate(all_numchange):
+                # num_add = args['num_steps'] / args['num_change'] - 1
+                num_add = args['num_steps'] / num_change - 1
+                for run in range(args['num_runs']):
+                    for seed in range(args['num_seeds']):
 
-                """build domain"""
-                domain = Ringworld(
-                    args['num_states'],
-                    left_probability=args['left_probability'],
-                    random_generator=np.random.RandomState(seed))
-                last_s, action, reward, gamma, s = domain.next(args['left_probability'])
-                last_x = domain.state_to_features(last_s)
-                x = domain.state_to_features(s)
-                left_probability = args['left_probability']
-                fre_count = np.zeros((args['num_states'], args['num_actions']))
+                        """build domain"""
+                        domain = Ringworld(
+                            args['num_states'],
+                            left_probability=args['left_probability'],
+                            random_generator=np.random.RandomState(seed))
+                        last_s, action, reward, gamma, s = domain.next(args['left_probability'])
+                        last_x = domain.state_to_features(last_s)
+                        x = domain.state_to_features(s)
+                        left_probability = args['left_probability']
+                        fre_count = np.zeros((args['num_states'], args['num_actions']))
 
-                """build learners"""
-                learner = GTD(last_x)
-                for step in range(args['num_steps']):
+                        """build learners"""
+                        learner = GTD(last_x)
+                        for step in range(args['num_steps']):
 
-                    # # compute the frequency for every 1000 steps
-                    # if step % args['num_frequency'] == 0:
-                    #     fre_behavior = np.ones((args['num_states'], args['num_actions']))
-                    #
-                    # # compute the probability
-                    # fre_behavior[last_s, action] += 1
+                            # # compute the frequency for every 1000 steps
+                            # if step % args['num_frequency'] == 0:
+                            #     fre_behavior = np.ones((args['num_states'], args['num_actions']))
+                            #
+                            # # compute the probability
+                            # fre_behavior[last_s, action] += 1
 
-                    # compute the frequency for every 1000 steps
+                            # compute the frequency for every 1000 steps
 
-                    if step % args['num_frequency'] == 0:
-                        fre_behavior = np.zeros((args['num_states'], args['num_actions']))
+                            if step % args['num_frequency'] == 0:
+                                fre_behavior = np.zeros((args['num_states'], args['num_actions']))
 
-                    # compute the probability
-                    fre_behavior[s, action] += 1
+                            # compute the count
+                            fre_behavior[s, action] += 1
 
-                    # update the probability
-                    # for i in range(args['num_actions']):
-                    #     if i == action:
-                    #         fre_count[last_s, i] += 1
-                    #     else:
-                    #         fre_count[last_s, i] *= args['decay']
+                            # update the probability
+                            # for i in range(args['num_actions']):
+                            #     if i == action:
+                            #         fre_count[last_s, i] += 1
+                            #     else:
+                            #         fre_count[last_s, i] *= args['decay']
 
-                    for i in range(args['num_actions']):
-                        if i == action:
-                            fre_count[s, i] += 1
-                        else:
-                            fre_count[s, i] *= args['decay']
+                            # update the count
+                            for i in range(args['num_actions']):
+                                if i == action:
+                                    fre_count[s, i] += 1
+                                else:
+                                    fre_count[s, i] *= decay
 
-                    # update behavior policy
-                    if step > 0 and step % args['num_change'] == 0:
-                        left_probability += (args['left_probability_end'] - args['left_probability'])/num_add
-                        print('left_probability', left_probability)
+                            # update behavior policy
+                            # if step > 0 and step % args['num_change'] == 0:
+                            if step > 0 and step % num_change == 0:
+                                left_probability += (args['left_probability_end'] - args['left_probability'])/num_add
+                                # print('left_probability', left_probability)
 
-                    # set message for siginfo
-                    siginfo_message = '[{0:3.2f}%] SEED: {1} of {2}, EPISODE: {3} of {4}, STEP: {5}'.format(
-                        100 * ((seed + step / args['num_steps']) / args['num_seeds']),
-                        seed + 1, args['num_seeds'], step + 1, args['num_steps'], step)
+                            # set message for siginfo
+                            siginfo_message = '[{0:3.2f}%] SEED: {1} of {2}, EPISODE: {3} of {4}, STEP: {5}'.format(
+                                100 * ((seed + step / args['num_steps']) / args['num_seeds']),
+                                seed + 1, args['num_seeds'], step + 1, args['num_steps'], step)
 
-                    # compute in the same way
-                    if option == 0:
-                        # print('unknown')
-                        if action == domain.LEFT:
-                            rho = 0.05 / args['left_probability']
-                        else:
-                            rho = 0.95 / (1 - args['left_probability'])
-                    # compute with the changed probability
-                    elif option == 1:
-                        # print('right rho')
-                        if action == domain.LEFT:
-                            rho = 0.05 / left_probability
-                        else:
-                            rho = 0.95 / (1 - left_probability)
-                    # compute with the decay count
-                    elif option == 2:
-                        # print('decay')
-                        if action == domain.LEFT:
-                            rho = 0.05 / (fre_count[s, action]/sum(fre_count[s, :]))
-                        else:
-                            rho = 0.95 / (fre_count[s, action]/sum(fre_count[s, :]))
-                    # compute the count with a siding window
-                    else:
-                        # print('count')
-                        if action == domain.LEFT:
-                            rho = 0.05 / (fre_behavior[s, action]/sum(fre_behavior[s, :]))
-                            # print('0', fre_behavior[s, action]/sum(fre_behavior[s, :]))
-                        else:
-                            rho = 0.95 / (fre_behavior[s, action]/sum(fre_behavior[s, :]))
-                            # print('1', fre_behavior[s, action]/sum(fre_behavior[s, :]))
+                            """compute the importance sampling rho"""
+                            # compute in the same way
+                            if option == 0:
+                                # print('unknown')
+                                if action == domain.LEFT:
+                                    rho = 0.05 / args['left_probability']
+                                else:
+                                    rho = 0.95 / (1 - args['left_probability'])
+                            # compute with the changed probability
+                            elif option == 1:
+                                # print('right rho')
+                                if action == domain.LEFT:
+                                    rho = 0.05 / left_probability
+                                else:
+                                    rho = 0.95 / (1 - left_probability)
+                            # compute with the decay count
+                            elif option == 2:
+                                # print('decay')
+                                if action == domain.LEFT:
+                                    rho = 0.05 / (fre_count[s, action]/sum(fre_count[s, :]))
+                                else:
+                                    rho = 0.95 / (fre_count[s, action]/sum(fre_count[s, :]))
+                            # compute the count with a siding window
+                            else:
+                                # print('count')
+                                if action == domain.LEFT:
+                                    rho = 0.05 / (fre_behavior[s, action]/sum(fre_behavior[s, :]))
+                                    # print('0', fre_behavior[s, action]/sum(fre_behavior[s, :]))
+                                else:
+                                    rho = 0.95 / (fre_behavior[s, action]/sum(fre_behavior[s, :]))
+                                    # print('1', fre_behavior[s, action]/sum(fre_behavior[s, :]))
 
-                    learner.update(reward, gamma, x, args['alpha'], args['eta'], args['lambda'], rho=rho)
+                            learner.update(reward, gamma, x, args['alpha'], args['eta'], args['lambda'], rho=rho)
 
-                    # record rmse and lambda :: compute return target policy
-                    all_rmse[option, run, seed, step] = domain.rmse(learner, left_probability=args['left_probability'])
-                    # all_rmse[option, seed, step] = domain.rmse(learner, left_probability=0.05)
+                            # record rmse and lambda :: compute return target policy
+                            all_rmse[option, decayInd, changeInd, run, seed, step] = domain.rmse(learner, left_probability=args['left_probability'])
+                            # all_rmse[option, seed, step] = domain.rmse(learner, left_probability=0.05)
 
-                    # move to next step :: with different behavior policy
-                    last_s, action, reward, gamma, s = domain.next(left_probability)
+                            # move to next step :: with different behavior policy
+                            last_s, action, reward, gamma, s = domain.next(left_probability)
 
-                    # if step < args['num_steps']//3:
-                    #     last_s, action, reward, gamma, s = domain.next(args['left_probability'])
-                    # else:
-                    #     last_s, action, reward, gamma, s = domain.next(args['left_probability2'])
+                            # if step < args['num_steps']//3:
+                            #     last_s, action, reward, gamma, s = domain.next(args['left_probability'])
+                            # else:
+                            #     last_s, action, reward, gamma, s = domain.next(args['left_probability2'])
 
-                    last_x = domain.state_to_features(last_s)
-                    x = domain.state_to_features(s)
+                            last_x = domain.state_to_features(last_s)
+                            x = domain.state_to_features(s)
 
     with open('{}/rmse_{}.npy'.format(args['directory'], args['test_name']), 'wb') as outfile:
         np.save(outfile, all_rmse)
@@ -229,13 +236,13 @@ def parse_args():
     parser.add_argument('--left_probability_end', type=float, dest='left_probability_end', default=0.95)
     parser.add_argument('--left_probability2', type=float, dest='left_probability2', default=0.75)
     parser.add_argument('--num_seeds', type=int, dest='num_seeds', default=100)
-    parser.add_argument('--num_runs', type=int, dest='num_runs', default=50)
+    parser.add_argument('--num_runs', type=int, dest='num_runs', default=1)
     parser.add_argument('--num_states', type=int, dest='num_states', default=5)
     parser.add_argument('--num_actions', type=int, dest='num_actions', default=2)
     parser.add_argument('--num_steps', type=int, dest='num_steps', default=50000)
     parser.add_argument('--num_frequency', type=int, dest='num_frequency', default=1000)
     parser.add_argument('--num_change', type=int, dest='num_change', default=1000)  # every 5000 steps behavior policy add
-    parser.add_argument('--test_name', default='four_agents_test')
+    parser.add_argument('--test_name', default='four_agents_test_different_change')
     args = vars(parser.parse_args())
     if 'num_steps' not in args:
         args['num_steps'] = args['num_states'] * 100
