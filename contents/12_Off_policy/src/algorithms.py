1478744c8a92b745c5bce0ed54a20d9982230355
hzm2016 <2539722953@qq.com>
2018-07-11 09:26:37 -0600

    update

--- contents/12_Off_policy/src/algorithms.py
+++ contents/12_Off_policy/src/algorithms.py
@@ -1,46 +1,354 @@
 # -*- coding: ascii -*-
 
 import numpy as np
+from typing import List, Tuple, Union
 
-from typing import Tuple
 
-
-__all__ = ('GTD', 'TIDBD', 'LGGTD', 'DLGGTD', 'LGTIDBD', 'DLGTIDBD', 'LGGTD2', 'DLGGTD2')
+__all__ = ('GTD', 'DiscreteActorCritic', 'OffActorCritic', 'OffPAC', 'TIDBD', 'LGGTD', 'DLGGTD', 'LGTIDBD', 'DLGTIDBD', 'LGGTD2', 'DLGGTD2')
 
 
 class GTD:
 
-    def __init__(self, initial_x: np.ndarray):
-        self.e = np.copy(initial_x)
+    def __init__(self,
+                 n: int,
+                 gamma: float,
+                 eta: float,
+                 alpha: float,
+                 lamda: float,
+                 ):
+        self.e = np.zeros(n, dtype=float)
         self.w = np.zeros(self.e.shape, dtype=float)
         self.h = np.zeros(self.e.shape, dtype=float)
+        self.gamma = gamma
+        self.alpha = alpha
+        self.eta = eta
+        self.lamda = lamda
         self.last_prediction = 0
         self.saved_auxiliary = 0
 
-    def predict(self, x: np.ndarray) -> float:
+    def predict(self,
+                x: np.ndarray) -> float:
         """Return the current prediction for a given set of features x."""
         return float(np.dot(self.w, x))
 
+    def start(self,
+              initial_x: np.ndarray):
+        self.e = np.copy(initial_x)
+
     def update(self,
                reward: float,
-               gamma: float,
                x: np.ndarray,
-               alpha: float,
-               eta: float,
-               lamda: float,
                rho: float=1,
                replacing: bool=False) -> float:
-        delta = reward + gamma * self.predict(x) - self.last_prediction
+        delta = reward + self.gamma * self.predict(x) - self.last_prediction
         self.e *= rho
-        self.w += alpha * (delta * self.e - gamma * (1 - lamda) * x * np.dot(self.e, self.h))
-        self.h += alpha * eta * (delta * self.e - self.saved_auxiliary)
-        self.e *= lamda * gamma
+        self.w += self.alpha * (delta * self.e - self.gamma * (1 - self.lamda) * x * np.dot(self.e, self.h))
+        self.h += self.alpha * self.eta * (delta * self.e - self.saved_auxiliary)
+        self.e *= self.lamda * self.gamma
         self.e += x
         if replacing:
             self.e = np.clip(self.e, 0, 1)
         self.last_prediction = self.predict(x)
         self.saved_auxiliary = np.dot(self.h, x) * x
         return delta
+
+
+"""Emphatic Actor Critic and Off-policy actor critic"""
+class OffActorCritic:
+
+    def __init__(self, n: int, num_actions: int,
+                 gamma: float,
+                 eta: float,
+                 alpha_v: float,
+                 alpha_u: float,
+                 lamda_v: float,
+                 lamda_u: float):
+
+        assert (n > 0)
+        assert (num_actions > 0)
+        self.num_actions = num_actions
+        self.random_generator = np.random
+        self.reward_bar = 0
+        self.e_v = np.zeros(n, dtype=float)
+        self.e_u = np.zeros((n, num_actions), dtype=float)
+        self.w_v = np.zeros(n, dtype=float)
+        self.w_u = np.zeros((n, num_actions), dtype=float)
+        self.h_v = np.zeros(self.e_v.shape, dtype=float)
+        self.last_action = 0
+        self.last_action_pro = np.zeros(num_actions, dtype=float)
+        self.last_prediction = 0
+        self.last_pho = 1
+        self.saved_auxiliary = 0
+        self.gamma = gamma
+        self.eta = eta
+        self.alpha_v = alpha_v
+        self.alpha_u = alpha_u
+        self.lamda_v = lamda_v
+        self.lamda_u = lamda_u
+        self.F = 0
+        self.initial = 1
+        self.M = 0
+        self.behavior_policy = np.zeros(num_actions, dtype=float)
+
+    def softmax(self, x: Union[List[float], np.ndarray]) -> np.ndarray:
+        x = np.asarray(x, dtype=float)
+        rv = np.zeros(self.num_actions)
+        for action in range(0, self.num_actions):
+            rv[action] = np.dot(x, (self.w_u[:, action]).flatten())
+        rv = rv - max(rv)
+        rv = np.exp(rv)
+        return rv / sum(rv)
+
+    def predict(self, x: Union[List[float], np.ndarray]):
+        x = np.asarray(x, dtype=float)
+        return np.dot(self.w_v, x)
+
+    def start(self,
+              initial_x: Union[List[float], np.ndarray],
+              behavior_policy: Union[List[float], np.ndarray]):
+        initial_x = np.asarray(initial_x, dtype=float)
+        pi = self.softmax(initial_x)
+        self.behavior_policy = np.asarray(behavior_policy, dtype=float)
+        action = self.random_generator.choice(self.num_actions, p=self.behavior_policy)
+        self.last_action_pro = pi
+        self.e_v += initial_x
+        self.e_u[:, action] += initial_x * (1 - sum(pi))
+        self.last_action = action
+        return action
+
+    def choose_action(self, x: Union[List[float], np.ndarray]):
+        x = np.asarray(x, dtype=float)
+        pi = self.softmax(x)
+        action = self.random_generator.choice(self.num_actions, p=pi)
+        return action
+
+    def step(self,
+             reward: float,
+             x: Union[List[float], np.ndarray],
+             behavior_policy: Union[List[float], np.ndarray]) -> Tuple[int, float]:
+        assert (0 <= self.gamma <= 1)
+        assert (self.eta >= 0)
+        assert (self.alpha_v > 0)
+        assert (self.alpha_u > 0)
+        assert (0 <= self.lamda_v <= 1)
+        assert (0 <= self.lamda_u <= 1)
+
+        x = np.asarray(x, dtype=float)
+        self.behavior_policy = np.array(behavior_policy)
+        self.F = self.last_pho * self.gamma * self.F + self.initial
+        self.M = (1 - self.lamda_u) * self.initial + self.lamda_u * self.F
+
+        pi = self.softmax(x)
+        action = self.random_generator.choice(self.num_actions, p=self.behavior_policy)
+
+        """compute the importance sampling"""
+        for j in range(self.num_actions):
+            if action == j:
+                pho = pi[j] / self.behavior_policy[j]
+
+        """Update the critic"""
+        prediction = np.dot(self.w_v, x)
+        delta = reward - self.reward_bar + self.gamma * prediction - self.last_prediction
+        self.w_v += self.alpha_v * pho * delta * x
+        self.reward_bar += self.eta * delta
+
+        """GTD"""
+        self.e_v *= pho
+        self.w_v += self.alpha_v * (delta * self.e_v - self.gamma * (1 - self.lamda_v) * x * np.dot(self.e_v, self.h_v))
+        self.h_v += self.alpha_v * self.eta * (delta * self.e_v - self.saved_auxiliary)
+        self.e_v *= self.lamda_v * self.gamma
+        self.e_v += x
+        # if replacing:
+        #     self.e = np.clip(self.e, 0, 1)
+
+        """Update the actor"""
+        # self.w_u += self.alpha_u * delta * self.e_u
+        self.w_u[:, action] += self.alpha_u * pho * self.M * delta * x
+        for other in range(self.num_actions):
+            self.w_u[:, other] -= self.alpha_u * pho * self.M * delta * x * pi[other]
+
+        """"""
+        self.last_pho = pho
+        self.last_action = action
+        self.last_action_pro = pi
+        self.last_prediction = prediction
+        self.saved_auxiliary = np.dot(self.h_v, x) * x
+        return action, float(delta)
+
+
+class OffPAC:
+
+    def __init__(self, n: int, num_actions: int,
+                 gamma: float,
+                 eta: float,
+                 alpha_v: float,
+                 alpha_u: float,
+                 alpha_h: float,
+                 lamda_v: float,
+                 lamda_u: float):
+
+        assert (n > 0)
+        assert (num_actions > 0)
+        self.num_actions = num_actions
+        self.random_generator = np.random
+        self.reward_bar = 0
+        self.e_v = np.zeros(n, dtype=float)
+        self.e_u = np.zeros((n, num_actions), dtype=float)
+        self.w_v = np.zeros(n, dtype=float)
+        self.w_u = np.zeros((n, num_actions), dtype=float)
+        self.h = np.zeros(self.e_v.shape, dtype=float)
+        self.last_action = 0
+        self.last_prediction = 0
+        self.saved_auxiliary = 0
+        self.gamma = gamma
+        self.eta = eta
+        self.alpha_v = alpha_v
+        self.alpha_u = alpha_u
+        self.alpha_h = alpha_h
+        self.lamda_v = lamda_v
+        self.lamda_u = lamda_u
+
+    def softmax(self, x: Union[List[float], np.ndarray]) -> np.ndarray:
+        x = np.asarray(x, dtype=float)
+        rv = np.zeros(self.num_actions)
+        for action in range(0, self.num_actions):
+            rv[action] = np.dot(x, (self.w_u[:, action]).flatten())
+        rv = rv - max(rv)
+        rv = np.exp(rv)
+        return rv / sum(rv)
+
+    def predict(self, x: Union[List[float], np.ndarray]):
+        x = np.asarray(x, dtype=float)
+        return np.dot(self.w_v, x)
+
+    def start(self, initial_x: Union[List[float], np.ndarray],
+              behavior_policy: Union[List[float], np.ndarray]):
+        initial_x = np.asarray(initial_x, dtype=float)
+        pi = self.softmax(initial_x)
+        action = self.random_generator.choice(self.num_actions, p=behavior_policy)
+        self.e_v += initial_x
+        self.e_u[:, action] += initial_x * (1 - sum(pi))
+        self.last_action = action
+        return action
+
+    def choose_action(self, x: Union[List[float], np.ndarray]):
+        x = np.asarray(x, dtype=float)
+        pi = self.softmax(x)
+        action = self.random_generator.choice(self.num_actions, p=pi)
+        return action
+
+    def step(self,
+             reward: float,
+             x: Union[List[float], np.ndarray],
+             behavior_policy: Union[List[float], np.ndarray]) -> Tuple[int, float]:
+        assert (0 <= self.gamma <= 1)
+        assert (self.eta >= 0)
+        assert (self.alpha_v > 0)
+        assert (self.alpha_u > 0)
+        assert (0 <= self.lamda_v <= 1)
+        assert (0 <= self.lamda_u <= 1)
+        x = np.asarray(x, dtype=float)
+        prediction = np.dot(self.w_v, x)
+        delta = reward - self.reward_bar + self.gamma * prediction - self.last_prediction
+        self.w_v += self.alpha_v * (delta * self.e_v - self.gamma * (1 - self.lamda_v) * x * np.dot(self.e_v, self.h))
+        self.h += self.alpha_h * (delta * self.e_v - self.saved_auxiliary)
+        self.w_u += self.alpha_u * delta * self.e_u
+
+        """choose action"""
+        pi = self.softmax(x)
+        action = self.random_generator.choice(self.num_actions, p=behavior_policy)
+
+        """compute the importance sampling"""
+        pho = pi[action] / np.array(behavior_policy[action])
+
+        self.reward_bar += self.eta * delta
+        self.e_v *= pho * self.lamda_v * self.gamma
+        self.e_v += pho * x
+        self.e_u *= pho * self.lamda_u * self.gamma
+        self.e_u[:, action] += pho * x
+        for other in range(self.num_actions):
+            self.e_u[:, other] -= pho * x * pi[other]
+        self.last_action = action
+        self.last_prediction = prediction
+        self.saved_auxiliary = np.dot(self.h, x) * x
+        return action, float(delta)
+
+
+class DiscreteActorCritic:
+
+    def __init__(self, n: int, num_actions: int,
+                 gamma: float,
+                 eta: float,
+                 alpha_v: float,
+                 alpha_u: float,
+                 lamda_v: float,
+                 lamda_u: float):
+
+        assert (n > 0)
+        assert (num_actions > 0)
+        self.num_actions = num_actions
+        self.random_generator = np.random
+        self.reward_bar = 0
+        self.e_v = np.zeros(n, dtype=float)
+        self.e_u = np.zeros((n, num_actions), dtype=float)
+        self.w_v = np.zeros(n, dtype=float)
+        self.w_u = np.zeros((n, num_actions), dtype=float)
+        self.last_action = 0
+        self.last_prediction = 0
+        self.gamma = gamma
+        self.eta = eta
+        self.alpha_v = alpha_v
+        self.alpha_u = alpha_u
+        self.lamda_v = lamda_v
+        self.lamda_u = lamda_u
+
+    def softmax(self, x: Union[List[float], np.ndarray]) -> np.ndarray:
+        x = np.asarray(x, dtype=float)
+        rv = np.zeros(self.num_actions)
+        for action in range(0, self.num_actions):
+            rv[action] = np.dot(x, (self.w_u[:, action]).flatten())
+        rv = np.exp(rv)
+        return rv / sum(rv)
+
+    def predict(self, x: Union[List[float], np.ndarray]):
+        x = np.asarray(x, dtype=float)
+        return np.dot(self.w_v, x)
+
+    def start(self, initial_x: Union[List[float], np.ndarray]):
+        initial_x = np.asarray(initial_x, dtype=float)
+        pi = self.softmax(initial_x)
+        action = self.random_generator.choice(self.num_actions, p=pi)
+        self.e_v += initial_x
+        self.e_u[:, action] += initial_x * (1 - sum(pi))
+        self.last_action = action
+        return action
+
+    def step(self,
+             reward: float,
+             x: Union[List[float], np.ndarray]) -> Tuple[int, float]:
+        assert (0 <= self.gamma <= 1)
+        assert (self.eta >= 0)
+        assert (self.alpha_v > 0)
+        assert (self.alpha_u > 0)
+        assert (0 <= self.lamda_v <= 1)
+        assert (0 <= self.lamda_u <= 1)
+        x = np.asarray(x, dtype=float)
+        prediction = np.dot(self.w_v, x)
+        delta = reward - self.reward_bar + self.gamma * prediction - self.last_prediction
+        self.w_v += self.alpha_v * delta * self.e_v
+        self.w_u += self.alpha_u * delta * self.e_u
+        pi = self.softmax(x)
+        action = self.random_generator.choice(self.num_actions, p=pi)
+        self.reward_bar += self.eta * delta
+        self.e_v *= self.lamda_v * self.gamma
+        self.e_v += x
+        self.e_u *= self.lamda_u * self.gamma
+        self.e_u[:, action] += x
+        for other in range(self.num_actions):
+            self.e_u[:, other] -= x * pi[other]
+        self.last_action = action
+        self.last_prediction = prediction
+        return action, float(delta)
 
 
 class TIDBD:
