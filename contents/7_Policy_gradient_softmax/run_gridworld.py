2b7e57b5416434a1ed2f42e1e5a909b07c5fdc28
hzm2016 <2539722953@qq.com>
2018-07-11 14:51:50 -0600

    update

--- contents/7_Policy_gradient_softmax/run_gridworld.py
+++ contents/7_Policy_gradient_softmax/run_gridworld.py
@@ -7,118 +7,216 @@
 # @Github    ： https://github.com/hzm2016
 """
 
-from Grid_world import Maze
 import numpy as np
-import math as m
+np.random.seed(1)
+import gym
+import gym_puddle
+import gym.spaces
+import pickle
+from LinearActorCritic import *
 from Tile_coding import *
-from LinearActorCritic import GTD
+import argparse
+
+"""Superparameters"""
+OUTPUT_GRAPH = True
+MAX_EPISODE = 5000
+DISPLAY_REWARD_THRESHOLD = 4001
+MAX_EP_STEPS = 5000
+
+"""Environments Informations :: Puddle world"""
+env = gym.make('PuddleWorld-v0')
+env.seed(1)
+env = env.unwrapped
+
+# print("Environments information:")
+# print(env.action_space.n)
+# print(env.observation_space.shape[0])
+# print(env.observation_space.high)
+# print(env.observation_space.low)
+
+"""Tile coding"""
+NumOfTilings = 10
+MaxSize = 100000
+HashTable = IHT(MaxSize)
+
+"""position and velocity needs scaling to satisfy the tile software"""
+PositionScale = NumOfTilings / (env.observation_space.high[0] - env.observation_space.low[0])
+VelocityScale = NumOfTilings / (env.observation_space.high[1] - env.observation_space.low[1])
+
+def getQvalueFeature(obv, action):
+    activeTiles = tiles(HashTable, NumOfTilings, [PositionScale * obv[0], VelocityScale * obv[1]], [action])
+    return activeTiles
+
+def getValueFeature(obv):
+    activeTiles = tiles(HashTable, NumOfTilings, [PositionScale * obv[0], VelocityScale * obv[1]])
+    return activeTiles
 
 
-def feature_representation(observation):
-    weight = np.array([[0.009, 0.008, 0.007, 0.006, 0.005, 0.004, 0.003, 0.002, 0.001], [0.001, 0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009]])
-    bias = np.array([0.0005, 0.0005, 0.0005, 0.0005, 0.0005, 0.0005, 0.0005, 0.0005, 0.0005])
-    return np.array(np.matrix(observation) * np.matrix(weight) + bias)[0]
+"""Parameters"""
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', default='../logs')
+    parser.add_argument('--alpha', type=float, default=np.array([5e-5, 5e-4, 1e-3, 1e-2, 0.5, 1.]))
+    parser.add_argument('--alpha_h', type=float, default=np.array([0.0001]))
+    parser.add_argument('--eta', type=float, default=0.0)
+    parser.add_argument('--lambda', type=float, default=np.array([0., 0.2, 0.4, 0.6, 0.8, 0.99]))
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--decay', type=float, default=0.99)
+    parser.add_argument('--ISW', type=int, default=0)
+    parser.add_argument('--left_probability', type=float, dest='left_probability', default=0.05)
+    parser.add_argument('--left_probability_end', type=float, dest='left_probability_end', default=0.75)
+    parser.add_argument('--num_seeds', type=int, dest='num_seeds', default=100)
+    parser.add_argument('--num_runs', type=int, dest='num_runs', default=50)
+    parser.add_argument('--num_states', type=int, dest='num_states', default=5)
+    parser.add_argument('--num_actions', type=int, dest='num_actions', default=2)
+    parser.add_argument('--num_episodes', type=int, dest='num_episodes', default=5000)
+    parser.add_argument('--num_frequency', type=int, dest='num_frequency', default=1000)
+    parser.add_argument('--num_change', type=int, dest='num_change', default=1000)
+    parser.add_argument('--all_algorithms', type=str, dest='all_algorithms', default=['DiscreteActorCritic', 'Allactions', 'AdvantageActorCritic'])
+    parser.add_argument('--behavior_policy', type=float, dest='behavior_policy', default=np.array([0.2, 0.2, 0.2, 0.2, 0.2]))
+    parser.add_argument('--target_policy', type=float, dest='target_policy',
+                        default=np.array([0., 0., 0.5, 0., 0.5]))
+    parser.add_argument('--test_name', default='puddle_control')
+    args = vars(parser.parse_args())
+    if 'num_steps' not in args:
+        args['num_steps'] = args['num_states'] * 100
+    return args
 
 
-def behavior_policy():
-    action = np.random.choice([0, 1, 2, 3], 1, p=[0.15, 0.35, 0.35, 0.15])
-    return action
+def play(LinearAC, agent):
 
+    if agent == 'Allactions':
 
-def target_policy():
-    action = np.random.choice([1, 2], 1, p=[0.5, 0.5])
-    return action
-
-
-def recongnizers_policy():
-    action = behavior_policy()
-    c = np.array([0., 1., 1, 0.])
-    if action == 1:
-        rec = 1 / np.sum(c * [0.15, 0.35, 0.35, 0.15])
-    elif action == 2:
-        rec = 1 / np.sum(c * [0.15, 0.35, 0.35, 0.15])
-    else:
-        rec = 0.
-    return action, rec
-
-
-def compute_true_return(gamma, state):
-    R = 0.
-    for i in range(16):
-        R += m.pow(gamma, i) * (-1)
-    R += m.pow(gamma, 16) * 10
-    return R
-
-
-def compute_mean_square_error(weight_name, state):
-    weight = np.load(weight_name)
-    true_value = compute_true_return(0.8, state)
-    pre_value = np.dot(weight, feature_representation(state))
-    pre_error = np.sqrt(np.power((pre_value - true_value), 2))
-    return pre_error
-
-
-def train(env):
-    delta = []
-    observation = env.reset()
-    learner = GTD(feature_representation(observation))
-    for episode in range(100):
-        # initial observation
+        t = 0
+        track_r = []
         observation = env.reset()
+        action = LinearAC.choose_action(getValueFeature(observation))
         while True:
-            # fresh env
-            env.render()
 
-            # RL choose action based on observation
-            # action = RL.choose_action(str(observation))
-            # action = behavior_policy()
-
-            """Compute the recongnizer"""
-            action, rho = recongnizers_policy()
-
-            # RL take action and get next observation and reward
-            observation_, reward, done = env.step(action)
-
-            """Compute the importance sampling: target policy: pi->0.5"""
-            # if action == 1:
-            #     rho = 0.5/0.25
-            # elif action == 2:
-            #     rho = 0.5/0.25
-            # else:
-            #     rho = 0
-
-            # RL learn from this transition
-            # RL.learn(str(observation), action, reward, str(observation_))
-            delta.append(learner.update(reward, 1.0, feature_representation(observation), 0.00005, 0.01, 0.8, rho))
-
-            # swap observation
+            observation_, reward, done, info = env.step(action)
+            track_r.append(reward)
+            feature = []
+            for i in range(env.action_space.n):
+                feature.append(getQvalueFeature(observation, i))
+            action, delta = LinearAC.step(reward, getValueFeature(observation), \
+                                          getQvalueFeature(observation, action), \
+                                          feature)
             observation = observation_
+            t += 1
+            if done or t > MAX_EP_STEPS:
+                return t, int(sum(track_r))
+    elif agent == 'AdvantageActorCritic':
 
-            # break while loop when end of this episode
-            if done:
-                break
+        t = 0
+        track_r = []
+        observation = env.reset()
+        action = LinearAC.choose_action(getValueFeature(observation))
+        while True:
 
-    # end of game
-    # np.save('delta', delta)
-    return learner
+            observation_, reward, done, info = env.step(action)
+
+            feature = []
+            for i in range(env.action_space.n):
+                feature.append(getQvalueFeature(observation, i))
+
+            # action_ = LinearAC.choose_action(getValueFeature(observation_))
+            # getQvalueFeature(observation_, action_),
+
+            track_r.append(reward)
+
+            action, delta = LinearAC.step(reward, getValueFeature(observation), \
+                                          getQvalueFeature(observation, action), \
+                                          feature)
+            observation = observation_
+            t += 1
+            if done or t > MAX_EP_STEPS:
+                return t, int(sum(track_r))
+    elif agent == 'Reinforce':
+
+        t = 0
+        track_r = []
+        observation = env.reset()
+        action = LinearAC.choose_action(getValueFeature(observation))
+        while True:
+
+            observation_, reward, done, info = env.step(action)
+            track_r.append(reward)
+            LinearAC.store_trasition(getValueFeature(observation), action, reward)
+            action = LinearAC.choose_action(getValueFeature(observation))
+            observation = observation_
+            t += 1
+            if done or t > MAX_EP_STEPS:
+
+                return t, int(sum(track_r))
+    elif agent == 'OffDiscreteActorCritic':
+
+        t = 0
+        track_r = []
+        observation = env.reset()
+        action = LinearAC.choose_action(getValueFeature(observation))
+        while True:
+
+            observation_, reward, done, info = env.step(action)
+            track_r.append(reward)
+            action, delta = LinearAC.step(reward, getValueFeature(observation))
+            observation = observation_
+            t += 1
+            if done or t > MAX_EP_STEPS:
+
+                return t, int(sum(track_r))
+    else:
+        t = 0
+        track_r = []
+        observation = env.reset()
+        action = LinearAC.choose_action(getValueFeature(observation))
+        while True:
+
+            observation_, reward, done, info = env.step(action)
+            track_r.append(reward)
+            action, delta = LinearAC.step(reward, getValueFeature(observation))
+            observation = observation_
+            t += 1
+            if done or t > MAX_EP_STEPS:
+
+                return t, int(sum(track_r))
 
 
-def run():
-    env = Maze()
-    print(env.reset())
-    GTD = train(env)
+if __name__ == '__main__':
+    args = parse_args()
 
-    value = np.zeros([10, 10])
-    for i in range(10):
-        for j in range(10):
-            value[i, j] = (GTD.predict(feature_representation(np.array([i, j]))))
+    """Run all the parameters"""
+    steps = np.zeros((len(args['all_algorithms']), len(args['lambda']), len(args['alpha']), args['num_runs'], args['num_episodes']))
+    rewards = np.zeros((len(args['all_algorithms']), len(args['lambda']), len(args['alpha']), args['num_runs'], args['num_episodes']))
+    for agentInd, agent in enumerate(args['all_algorithms']):
+        for lamInd, lam in enumerate(args['lambda']):
+            for alphaInd, alpha in enumerate(args['alpha']):
+                for run in range(args['num_runs']):
+                    if agent == 'Reinforce':
+                        LinearAC = Reinforce(MaxSize, env.action_space.n, args['gamma'], args['eta'], alpha*10, alpha, lam, lam)
+                    elif agent == 'Allactions':
+                        LinearAC = Allactions(MaxSize, env.action_space.n,  args['gamma'], args['eta'], alpha*10, alpha, lam, lam)
+                    elif agent == 'AdvantageActorCritic':
+                        LinearAC = AdvantageActorCritic(MaxSize, env.action_space.n,  args['gamma'], args['eta'], alpha*10, alpha, lam, lam)
+                    elif agent == 'DiscreteActorCritic':
+                        LinearAC = DiscreteActorCritic(MaxSize, env.action_space.n,  args['gamma'], args['eta'], alpha*10, alpha, lam, lam)
+                    else:
+                        print('Please give the right agent!')
+                    print("++++++++++++++++++++++%s++++++++++++++++++++" % agent)
+                    observation = env.reset()
+                    action = LinearAC.start(getValueFeature(observation))
+                    for ep in range(args['num_episodes']):
 
-    print(GTD.w)
-    np.save('weight', np.array(GTD.w))
-    print(value)
-    np.save('value', np.array(value))
+                        step, reward = play(LinearAC, agent)
+                        if ep == 0:
+                            running_reward = reward
+                        else:
+                            running_reward = running_reward * 0.99 + reward * 0.01
+                        steps[agentInd, lamInd, alphaInd, run, ep] = step
+                        rewards[agentInd, lamInd, alphaInd, run, ep] = running_reward
+                        print('agent %s, lambda %f, alpha %f, run %d, episode %d, steps %d, rewards%d' %
+                              (agent, lam, alpha, run, ep, step, running_reward))
+    with open('{}/step_{}.npy'.format(args['directory'], args['test_name']), 'wb') as outfile:
+        np.save(outfile, steps)
+    with open('{}/reward_{}.npy'.format(args['directory'], args['test_name']), 'wb') as outfile:
+        np.save(outfile, rewards)
 
-
-if __name__ == "__main__":
-
-    run()
