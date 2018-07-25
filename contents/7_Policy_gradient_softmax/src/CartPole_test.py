0fc5803f8c07e03732a67fa090e59bc297ba366e
hzm2016 <2539722953@qq.com>
2018-07-24 21:54:38 -0600

    upload figure

---
+++ contents/7_Policy_gradient_softmax/src/CartPole_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 14/07/18 9:29 AM
+# @Author  : ZHIMIN HOU
+# @FileName: CartPole_test.py
+# @Software: PyCharm
+# @Github    ： https://github.com/hzm2016
+"""
